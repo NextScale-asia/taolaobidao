bb43d543f6e27b4701d0f0ff6b67bbd1d0243032
Tạ Phước Ánh <taphuocanh94@gmail.com>
2024-11-19 17:28:31 +0700

    update

--- libs/helpers/tranlate.ts
+++ libs/helpers/tranlate.ts
@@ -1,14 +1,17 @@
+import * as viLang from '$locale/vi_VN/locale.json' with { type: 'json'}
+
 export default function translate(key_string: string, locale: string = "vi_VN") {
     const keys = key_string.split('.'); // Tách chuỗi thành mảng các key
-    const localePath = import.meta.resolve("$locale/").replace("file:///", "");
-    const localeText = Deno.readTextFileSync(`${localePath}${locale}/locale.json`);
-    const localeJson = JSON.parse(localeText);
+    // const localePath = import.meta.resolve("$locale/").replace("file:///", "");
+    // console.log(`${localePath}${locale}/locale.json`)
+    // const localeText = Deno.readTextFileSync(`${localePath}${locale}/locale.json`);
+    const localeJson = viLang.default //JSON.parse(localeText);
     
-    let translated = localeJson;
+    let translated: any = localeJson;
 
     for (const key of keys) {
-        if (translated[key] !== undefined) {
-            translated = translated[key]; // Tiếp tục duyệt qua các thuộc tính
+        if (key in translated) {
+            translated = translated[key as keyof typeof translated]; // Tiếp tục duyệt qua các thuộc tính
         } else {
             return key_string; // Trả về undefined nếu key không tồn tại
         }
